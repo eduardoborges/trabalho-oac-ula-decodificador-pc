62c058a090d93e6d7f623d6408bc4011e9c435e9
ismaelse13 <ismaelse13@outlook.com>
2017-03-27 20:01:55 -0300

    Correção da mascara negativa.

--- Decodificador.java
+++ Decodificador.java
@@ -1,18 +1,18 @@
  class Decodificador {
-
-	private final static int operatorMask 		= 0b11000000000000000000000000000000;
-	private final static int firstNumberMask 	= 0b00000000000000000000000011111111;
-	private final static int lastNumberMask 	= 0b00000000000000001111111100000000;
+	// 										      ---|--------------|------|--------
+	private final static int operatorMask 		= 0b01100000000000000000000000000000;
+	private final static int firstNumberMask 	= 0b00000000000000001111111100000000;
+	private final static int lastNumberMask 	= 0b00000000000000000000000011111111;
 
 	public static int getOperator(int number){
-		return (number & operatorMask) >> 30;
+		return (number & operatorMask) >> 29;
 	}
 
 	public static int getFirstNumber(int number){
-		return (number & firstNumberMask);
+		return (number & firstNumberMask) >> 8;
 	}
 
 	public static int getLastNumber(int number){
-		return (number & lastNumberMask) >> 8;
+		return (number & lastNumberMask);
 	}
 }
