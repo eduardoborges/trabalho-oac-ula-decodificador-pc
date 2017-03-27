7d412bebec2306570f1012041da7bf8ba345c87a
ismaelse13 <ismaelse13@outlook.com>
2017-03-27 20:02:46 -0300

    Alocaçao do metodo de execuçao para a classe Computador

--- Main.java
+++ Main.java
@@ -1,23 +1,14 @@
 class Main {
 	public static void main(String[] args) {
+		
 		int[] memoria = new int[]{
-			0b01000000000000000000101000000011, // 1 - 10 - 3
-			0b10000000000000000000001100001010, // 2 - 3 - 10
-			0b11000000000000000000010100001111  // 3 - 4 - 15
+			0b00100000000000000000101000000011, // 1 - 10 - 3
+			0b01000000000000000000001100001010, // 2 - 3 - 10
+			0b01100000000000000000010100001111  // 3 - 5 - 15
 		};
 
-		// Computador computador  = new Computador(memoria);
+		Computador maquina  = new Computador(memoria);
+		maquina.run();
 
-		for (int code:memoria) {
-
-			int operator 		= Decodificador.getOperator(code);
-			int firstNumber 	= Decodificador.getFirstNumber(code);
-			int lastNumber 		= Decodificador.getLastNumber(code);
-
-			System.out.println(operator + " " + firstNumber + " " + lastNumber);
-		}
-
-
-		
 	}
 }
